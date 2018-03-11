4f86d95240b880a059179ad1531eb3e74e575df3
Pierre <pierre@hubert.fr>
2018-03-11 18:51:04 +0100

    Display comments owner.

--- app/src/main/java/org/communiquons/android/comunic/client/ui/adapters/CommentsAdapter.java
+++ app/src/main/java/org/communiquons/android/comunic/client/ui/adapters/CommentsAdapter.java
@@ -6,11 +6,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.communiquons.android.comunic.client.R;
+import org.communiquons.android.comunic.client.data.ImageLoad.ImageLoadManager;
 import org.communiquons.android.comunic.client.data.UsersInfo.UserInfo;
 import org.communiquons.android.comunic.client.data.comments.Comment;
+import org.communiquons.android.comunic.client.data.utils.UiUtils;
 
 import java.util.ArrayList;
 
@@ -49,20 +52,35 @@ public class CommentsAdapter extends ArrayAdapter<Comment> {
         View v = LayoutInflater.from(context).inflate(R.layout.comment_item, viewGroup, false);
 
         //Return filled view
-        return fillView(v, comment, user);
+        return fillView(context, v, comment, user);
 
     }
 
     /**
      * Fill a view with a specified comments informations
      *
+     * @param context The context of the acivitiy / application
      * @param view The view to update
      * @param comment The comment to update
      * @param user Information about the user (NULL for none)
      * @return Updated view
      */
-    private static View fillView(View view, Comment comment, @Nullable UserInfo user){
+    private static View fillView(Context context, View view, Comment comment,
+                                 @Nullable UserInfo user){
 
+        //Update user name and account image
+        ImageView accountImage = view.findViewById(R.id.user_account_image);
+        TextView accountName = view.findViewById(R.id.user_account_name);
+
+        if(user == null){
+            accountImage.setImageDrawable(UiUtils.getDrawable(context,
+                    R.drawable.default_account_image));
+            accountName.setText("");
+        }
+        else {
+            ImageLoadManager.load(context, user.getAcountImageURL(), accountImage);
+            accountName.setText(user.getDisplayFullName());
+        }
 
         //Update comment content
         ((TextView)view.findViewById(R.id.comment_text)).setText(comment.getContent());
