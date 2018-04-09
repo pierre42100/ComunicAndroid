bcc747ee5f7c233374bb95068ec185c89f42fcdb
Pierre <pierre@hubert.fr>
2018-04-09 18:42:48 +0200

    Created Notif object

---
+++ app/src/main/java/org/communiquons/android/comunic/client/data/notifications/NotifElemType.java
@@ -0,0 +1,42 @@
+package org.communiquons.android.comunic.client.data.notifications;
+
+/**
+ * Notifications elements type
+ *
+ * @author Pierre HUBERT
+ * Created by pierre on 4/9/18.
+ */
+
+public enum NotifElemType {
+
+    /**
+     * On a post
+     */
+    POST,
+
+    /**
+     * On a conversation
+     */
+    CONVERSATION,
+
+    /**
+     * On a conversation message
+     */
+    CONVERSATION_MESSAGE,
+
+    /**
+     * On a user page
+     */
+    USER_PAGE,
+
+    /**
+     * On a comment
+     */
+    COMMENT,
+
+    /**
+     * On a friendship request
+     */
+    FRIEND_REQUEST
+
+}
---
+++ app/src/main/java/org/communiquons/android/comunic/client/data/notifications/NotificationTypes.java
@@ -0,0 +1,42 @@
+package org.communiquons.android.comunic.client.data.notifications;
+
+/**
+ * Enum of the notifications types
+ *
+ * @author Pierre HUBERT
+ * Created by pierre on 4/9/18.
+ */
+
+public enum NotificationTypes {
+
+    /**
+     * When a comment was created
+     */
+    COMMENT_CREATED,
+
+    /**
+     * When a friendship request was sent
+     */
+    SENT_FRIEND_REQUEST,
+
+    /**
+     * When a friendship request was accepted
+     */
+    ACCEPTED_FRIEND_REQUEST,
+
+    /**
+     * When a friend request was rejected
+     */
+    REJECTED_FRIEND_REQUEST,
+
+    /**
+     * When an elem has been created
+     */
+    ELEM_CREATED,
+
+    /**
+     * When an element is updated
+     */
+    ELEM_UPDATED
+
+}
